bc381f9c9ae56f1fec678ed3a5641611246bd104
urban1991 <urban199191@gmail.com>
2023-10-06 19:09:33 +0200

    client: added Layout component to _app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import {type AppType} from "next/app";
+
+import {api} from "$/utils/api";
+
+import "$/styles/globals.css";
+import {Layout} from "$/components/Layout";
+
+const MyApp: AppType = ({Component, pageProps}) => {
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
+};
+
+export default api.withTRPC(MyApp);
