44501df2ee9064c3b94fce1fa7b99adfa4710d23
urban1991 <urban199191@gmail.com>
2023-10-06 19:19:22 +0200

    client: update HomePage of the website

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import {PageHead} from "$/components/PageHead";
+import {Home} from "$/components/Home";
+
+export default function HomePage() {
+    return (
+        <>
+            <PageHead title="Strona główna" />
+
+            <Home />
+        </>
+    );
+}
